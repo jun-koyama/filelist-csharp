0bb69e3964fcf547d22ee0d24aaee807a22870e7
Jun <koyama@qahub.tv>
2017-06-30 11:10:39 +0900

    initial commit

---
+++ FileList/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace FileList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderNameSource = new System.Windows.Forms.TextBox();
+            this.openFolderSource = new System.Windows.Forms.Button();
+            this.openFolderTarget = new System.Windows.Forms.Button();
+            this.folderNameTarget = new System.Windows.Forms.TextBox();
+            this.execute = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // folderNameSource
+            // 
+            this.folderNameSource.Location = new System.Drawing.Point(138, 17);
+            this.folderNameSource.Name = "folderNameSource";
+            this.folderNameSource.Size = new System.Drawing.Size(332, 19);
+            this.folderNameSource.TabIndex = 0;
+            // 
+            // openFolderSource
+            // 
+            this.openFolderSource.Location = new System.Drawing.Point(478, 17);
+            this.openFolderSource.Name = "openFolderSource";
+            this.openFolderSource.Size = new System.Drawing.Size(61, 18);
+            this.openFolderSource.TabIndex = 1;
+            this.openFolderSource.Text = "open";
+            this.openFolderSource.UseVisualStyleBackColor = true;
+            this.openFolderSource.Click += new System.EventHandler(this.openFolderSource_Click);
+            // 
+            // openFolderTarget
+            // 
+            this.openFolderTarget.Location = new System.Drawing.Point(478, 52);
+            this.openFolderTarget.Name = "openFolderTarget";
+            this.openFolderTarget.Size = new System.Drawing.Size(61, 19);
+            this.openFolderTarget.TabIndex = 2;
+            this.openFolderTarget.Text = "open";
+            this.openFolderTarget.UseVisualStyleBackColor = true;
+            this.openFolderTarget.Click += new System.EventHandler(this.openFolderTarget_Click);
+            // 
+            // folderNameTarget
+            // 
+            this.folderNameTarget.Location = new System.Drawing.Point(138, 52);
+            this.folderNameTarget.Name = "folderNameTarget";
+            this.folderNameTarget.Size = new System.Drawing.Size(332, 19);
+            this.folderNameTarget.TabIndex = 3;
+            // 
+            // execute
+            // 
+            this.execute.Location = new System.Drawing.Point(451, 97);
+            this.execute.Name = "execute";
+            this.execute.Size = new System.Drawing.Size(88, 31);
+            this.execute.TabIndex = 4;
+            this.execute.Text = "execute";
+            this.execute.UseVisualStyleBackColor = true;
+            this.execute.Click += new System.EventHandler(this.execute_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ソースファイルルート";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "クラスファイルルート";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 141);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.execute);
+            this.Controls.Add(this.folderNameTarget);
+            this.Controls.Add(this.openFolderTarget);
+            this.Controls.Add(this.openFolderSource);
+            this.Controls.Add(this.folderNameSource);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox folderNameSource;
+        private System.Windows.Forms.Button openFolderSource;
+        private System.Windows.Forms.Button openFolderTarget;
+        private System.Windows.Forms.TextBox folderNameTarget;
+        private System.Windows.Forms.Button execute;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FileList/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FileList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void execute_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(this.folderNameSource.Text) || String.IsNullOrEmpty(this.folderNameTarget.Text))
+            {
+                MessageBox.Show("フォルダが指定されていません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (Execute())
+            {
+                MessageBox.Show("正常終了しました。", "情報", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("異常終了しました。", "情報", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void openFolderSource_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            folderBrowserDialog.Description = "フォルダを指定してください。";
+            folderBrowserDialog.RootFolder = Environment.SpecialFolder.Desktop;
+            folderBrowserDialog.ShowNewFolderButton = true;
+
+            if (folderBrowserDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                this.folderNameSource.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void openFolderTarget_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            folderBrowserDialog.Description = "フォルダを指定してください。";
+            folderBrowserDialog.RootFolder = Environment.SpecialFolder.Desktop;
+            folderBrowserDialog.ShowNewFolderButton = true;
+
+            if (folderBrowserDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                this.folderNameTarget.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private bool Execute()
+        {
+            System.IO.DirectoryInfo directoryInfo = new System.IO.DirectoryInfo(this.folderNameSource.Text);
+            System.IO.FileInfo[] files = directoryInfo.GetFiles("*.java", System.IO.SearchOption.AllDirectories);
+
+            string fileName = GetSaveFileName();
+            if (!String.IsNullOrEmpty(fileName))
+            {
+                WriteCSV(fileName, files);
+                CopyClassFile(files);
+            }
+            return true;
+        }
+
+        private string GetSaveFileName()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = "result.csv";
+            saveFileDialog.InitialDirectory = @"C:\";
+            saveFileDialog.Filter = "CSV Files (.csv)|*.csv";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.Title = "保存先のファイルを選択してください";
+            saveFileDialog.RestoreDirectory = true;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                return saveFileDialog.FileName;
+            }
+            return null;
+        }
+
+        private bool WriteCSV(string fileName, System.IO.FileInfo[] files)
+        {
+            using (var sw = new System.IO.StreamWriter(fileName))
+            {
+                foreach (System.IO.FileInfo file in files)
+                {
+                    sw.WriteLine("{0}, {1}", file.Name, file.LastWriteTime.ToString("yyyy/MM/dd hh:mm:ss"));
+                }
+            }
+            return true;
+        }
+
+        private bool CopyClassFile(System.IO.FileInfo[] files)
+        {
+            foreach (System.IO.FileInfo file in files)
+            {
+                // Path.GetExtentionでした方がよかったかな
+                string fileName = file.Name.Substring(0, file.Name.LastIndexOf(".java")) + ".class";
+                string sourcePath = file.DirectoryName.Replace(this.folderNameSource.Text, this.folderNameTarget.Text) + System.IO.Path.DirectorySeparatorChar + fileName;
+                string copyToPath = file.DirectoryName + System.IO.Path.DirectorySeparatorChar + fileName;
+                System.IO.File.Copy(sourcePath, copyToPath, false);
+                System.IO.File.Delete(file.FullName);
+            }
+            return true;
+        }
+    }
+}
